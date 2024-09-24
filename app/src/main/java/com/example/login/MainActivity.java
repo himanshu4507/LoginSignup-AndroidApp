f813e0883eabf011560b8772fd663543df3c671b
himanshu4507 <himanshuchaudhary4507@gmail.com>
2024-09-24 23:21:35 +0530

    initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.login;
+
+import static com.example.login.R.id.log_In;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView log_In;
+        log_In=findViewById(R.id.log_In);
+        String text=getString(R.string.Account);
+        SpannableString spannableString = new SpannableString(text);
+
+        ClickableSpan clickableSpan=new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Intent intent =new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false); // Remove underline
+                ds.setColor(Color.BLUE); // Set text color
+            }
+        };
+        int start=text.indexOf("Log In");
+        int end=start+ "Log In".length();
+        spannableString.setSpan(clickableSpan,start,end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        log_In.setText(spannableString);
+        log_In.setMovementMethod(LinkMovementMethod.getInstance());
+        log_In.setHighlightColor(Color.TRANSPARENT);
+    }
+}
