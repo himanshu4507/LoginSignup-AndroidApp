e9f4cc02aa06d70d68255283bdcf8a0b8d5af0ef
himanshu4507 <himanshuchaudhary4507@gmail.com>
2024-09-24 23:37:00 +0530

    SignInSignUp-App

---
+++ app/src/main/java/com/example/login/MainActivity2.java
@@ -0,0 +1,59 @@
+package com.example.login;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView signup_text;
+        signup_text=findViewById(R.id.signup_text);
+        String text=getString(R.string.new_here_sign_up);
+        SpannableString spannableString = new SpannableString(text);
+
+        ClickableSpan clickableSpan=new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Intent intent =new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(intent);
+            }
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false); // Remove underline
+                ds.setColor(Color.BLUE); // Set text color
+            }
+        };
+       int start=text.indexOf("Sign Up");
+       int end=start+ "Sign Up".length();
+       spannableString.setSpan(clickableSpan,start,end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        signup_text.setText(spannableString);
+        signup_text.setMovementMethod(LinkMovementMethod.getInstance());
+        signup_text.setHighlightColor(Color.TRANSPARENT);
+    }
+}
